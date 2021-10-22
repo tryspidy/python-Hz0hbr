acdd423754c1bf425d2278545184cbf7e67726e9
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-22 12:22:43 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+Python is an interpreted, high-level, 
+general-purpose programming language.
+
+//as you can also see to your right --------------------->
+
+but also note interpreted, not compiled. 
